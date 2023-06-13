37dc147a16c6496134562a469404a4c0f2a26957
os-tap <bryzgalov.ov@phystech.edu>
2023-06-13 12:46:19 +0300

    init

---
+++ build/src/CMakeFiles/SDLTemplate.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDLTemplate.dir/Window.cc.o"
+  "CMakeFiles/SDLTemplate.dir/Window.cc.o.d"
+  "CMakeFiles/SDLTemplate.dir/main.cpp.o"
+  "CMakeFiles/SDLTemplate.dir/main.cpp.o.d"
+  "SDLTemplate"
+  "SDLTemplate.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDLTemplate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
